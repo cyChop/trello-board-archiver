c23fdd8934d39d152636e4f52606f43a6f0f9d9e
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2018-08-18 13:56:56 +0200

    Initial commit from Spring Boot Initializr

---
+++ src/main/java/org/keyboardplaying/trello/archiver/TrelloBoardArchiverApplication.java
@@ -0,0 +1,12 @@
+package org.keyboardplaying.trello.archiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloBoardArchiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloBoardArchiverApplication.class, args);
+	}
+}
